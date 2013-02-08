98fd0ac216bd4eef10bc4168f08e16f77ab2ff56
Marcelo Choque <jukumaro@gmail.com>
2013-02-08 14:11:09 -0400

    Inicializamos el proyecto

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,5 @@
+class WelcomeController < ApplicationController
+  def index
+  	@nombre="Marcelo Choque"
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HolaMundo::Application.config.secret_token = '06851b63c6cbeb9f139780246c504c5f36ececbbc27fa6da8f0254395afa23ab9d874726af1ddaa57fb00781b8671a15010a17fe279f1dbea0497efa117be548'
